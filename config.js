2aa9cc0cff392b7d6e059ed01fdfcd5d4b5c1b27
jjarj83 <33990260+jjarj83@users.noreply.github.com>
2022-12-22 10:28:37 -0500

    Add files via upload
    u

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+  "union-flats": {
+    "name": "Union Flats",
+    "address": "415 Center Pointe Dr, Mechanicsburg, PA 17050",
+    "apartments_link": "https://www.apartments.com/union-flats-mechanicsburg-pa/qhdlwhx/",
+    "website_link": "https://www.unionflatspa.com/",
+    "type": "apartment",
+    "rent": "$1,525/mo",
+    "beds": 2,
+    "baths": 2
+  }
+};
---
+++ generate_page.js
@@ -0,0 +1,8 @@
+var apartment = config['union-flats'];
+console.log(apartment);
+
+generateHeader();
+
+function generateHeader() {
+  document.title = apartment.name;
+}
